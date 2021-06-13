3f22ed3dde8e3acd7e8a65e14286b01da9051503
minkeun <minkeun428@gmail.com>
2021-06-14 02:20:32 +0900

    mysql mybatis 연동

--- src/main/java/com/webapp/StartApplication.java
+++ src/main/java/com/webapp/StartApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication   // 가장 먼저 실행되어야 할 클래스를 알려준다.
 public class StartApplication {
     public static void main(String[] args) {
-        //이 프로젝트를 실행하겠다. -> 내장 WAS 실행. -> 스프링부트로 만들어진 Jar파일 실 (spring-boot-stater-tomcat)
+        //이 프로젝트를 실행하겠다. -> 내장 WAS 실행. -> 스프링부트로 만들어진 Jar파일 실행 (spring-boot-stater-tomcat)
         SpringApplication.run(StartApplication.class, args);
-        System.out.println("Hello!!");
     }
 }
