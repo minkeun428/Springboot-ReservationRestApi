8762491cc9b31687cd4522407bf9c4296a585ed8
minkeun <minkeun428@gmail.com>
2021-06-14 15:44:26 +0900

    Service 생성

---
+++ src/main/java/com/webapp/service/ReservationService.java
@@ -0,0 +1,22 @@
+package com.webapp.service;
+
+import com.webapp.dto.ReservationDto;
+import org.apache.ibatis.annotations.*;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
+public interface ReservationService {
+
+    List<ReservationDto> getReservationList();
+
+    ReservationDto getReservation(@Param("id") String id);
+
+    int insertReservation(@Param("id") String id, @Param("name") String name, @Param("phone") String phone, @Param("peopleCount") int peopleCount);
+
+    int updateReservation(@Param("id") String id, @Param("name") String name, @Param("phone") String phone, @Param("peopleCount") int peopleCount);
+
+    int deleteReservation(@Param("id") String id);
+
+}
